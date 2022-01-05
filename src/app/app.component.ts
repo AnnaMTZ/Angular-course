b7de973dacd4a965f21a45e831427780fc8a9753
CORP\ANMYKHAI <anna.mykhailenko@capgemini.com>
2022-01-05 20:41:55 +0100

    first assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', ///should be unique
+  // selector: '[app-root]',
+  // selector: 'app-servers',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: pink;
+    }
+    `]
 })
 export class AppComponent {
-  title = 'angular-app';
+
 }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+// First of all, a component simply is just a class, a TypeScript class, so that Angular is able 
+// to instantiate it to create objects based on the blueprint we set up here you could say.
+// Decorators are ts features which help you to enhance your classes or elements in your code
+
+import { Component } from "@angular/core"; /// to import a @Component decorator
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
